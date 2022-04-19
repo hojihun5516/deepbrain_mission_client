595e3d55fc1a809dc03065611e438d1fbdb9da47
hojihun5516 <hojihun5516@daum.net>
2022-04-19 16:23:25 +0900

    feat: Add board list and create table

---
+++ pages/board/board-list.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import tableStyles from "../common/styles/table.module.css";
+export default function BoardList() {
+  const columns = [
+    "업종명",
+    "인허가일자",
+    "업소명",
+    "소재지(도로명)",
+    "소재지(지번)",
+    "영업장면적",
+    "건물내부면적",
+    "건물외부면적",
+    "소재지전화",
+    "업태명",
+  ];
+  return (
+    <table className={tableStyles.table}>
+      <thead>
+        <tr>
+          <th colSpan={columns.length}>
+            <h2>공원 목록</h2>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          {columns.map((column) => (
+            <td key={column}>{column}</td>
+          ))}
+        </tr>
+        <tr style={{ height: 200 }}>
+          <td colSpan={columns.length}>준비중입니다.</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
